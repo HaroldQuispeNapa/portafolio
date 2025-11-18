aafaa23ecbd19387a5a32d16fe0e994d390254ab
Harold Quispe <efrainqn16@gmail.com>
2025-11-18 01:35:54 -0500

    implementando el servicio de project

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,11 @@ import { EducationModule } from './education/education.module';
 import { ExperienceModule } from './experience/experience.module';
 import { LanguageModule } from './language/language.module';
 import { SkillModule } from './skill/skill.module';
+import { ProjectModule } from './project/project.module';
+import { BlogModule } from './blog/blog.module';
 
 @Module({
-  imports: [ProfileModule, ResumeModule, ActivityModule, SoftSkillModule, EducationModule, ExperienceModule, LanguageModule, SkillModule],
+  imports: [ProfileModule, ResumeModule, ActivityModule, SoftSkillModule, EducationModule, ExperienceModule, LanguageModule, SkillModule, ProjectModule, BlogModule],
   controllers: [],
   providers: [],
 })
