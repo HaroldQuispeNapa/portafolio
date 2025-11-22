c9dba44ed73567344f325fffb1bbe1947436ee14
Harold Quispe <efrainqn16@gmail.com>
2025-11-22 03:01:29 -0500

    inyectando dependencia de aws

--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -2,33 +2,57 @@ import { Injectable, OnModuleInit } from '@nestjs/common';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
 import { PrismaClient } from '@prisma/client';
+import { UploaderService } from 'src/services/uploader/s3.service';
+import { v4 } from 'uuid';
 
 //TODO: AGREGAR SERVICIO DE IMAGENES
 
 @Injectable()
 export class ProjectService extends PrismaClient implements OnModuleInit {
+  constructor(private readonly uploadService: UploaderService) {
+    super();
+  }
 
   async onModuleInit() {
     await this.$connect();
   }
 
-  create(createProjectDto: CreateProjectDto, image: Express.Multer.File) {
+  async create(createProjectDto: CreateProjectDto, image: Express.Multer.File) {
     console.log(image);
+    const key = v4();
+    const project = await this.project.create({
+      data: {
+        ...createProjectDto,
+        image: key,
+      },
+    });
+
+    await this.uploadService.upload(image, key);
+    return project;
   }
 
-  findAll() {
-    return this.project.findMany({
+  async findAll() {
+    const projects = await this.project.findMany({
       orderBy: {
         createdAt: 'desc',
       },
     });
+
+    const projectsWithImage = await Promise.all(
+      projects.map(async (project) => {
+        const url = await this.uploadService.getSignedUrl(project.image!);
+        return { ...project, image: url };
+      }),
+    );
+
+    return projectsWithImage;
   }
 
   findOne(id: string) {
     return this.project.findFirst({
       where: {
-        id
-      }
+        id,
+      },
     });
   }
 
@@ -36,7 +60,9 @@ export class ProjectService extends PrismaClient implements OnModuleInit {
     return this.project.update({ where: { id }, data: updateProjectDto });
   }
 
-  remove(id: string) {
-    return this.project.delete({ where: { id } });
+  async remove(id: string) {
+    const project = await this.project.delete({ where: { id } });
+    await this.uploadService.delete(project.image!);
+    return project;
   }
 }
